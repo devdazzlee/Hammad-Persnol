ba5fa2384145afdef4f5e97afd869dfb059381bb
Ahmed Raza <101836398+devdazzlee@users.noreply.github.com>
2025-07-14 00:07:46 +0500

    Add Techsol-Mail-Api code as part of main repo

---
+++ Techsol-Mail-Api/middlewares/errorHandler.js
@@ -0,0 +1,9 @@
+// middlewares/errorHandler.js
+
+export default (err, req, res, next) => {
+    console.error('Error:', err);
+    res.status(500).json({
+      error: `An unexpected error occurred. Please try again later. ${err}`,
+    });
+  };
+  
